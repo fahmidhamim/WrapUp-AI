0c1a4d9e0ed36fe0949163b741698e8fb7f36afc
Fahmid Hossain Hamim <fh.hamim18@gmail.com>
2026-04-17 12:52:18 +0600

    Revert "feat: remove stats section from landing page"
    This reverts commit 82ef99c56ac6e012515343ab77f671a969651591.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import StarfieldBackground from "@/components/landing/StarfieldBackground";
 import TrustedBySection from "@/components/landing/TrustedBySection";
 import FeaturesSection from "@/components/landing/FeaturesSection";
 import HowItWorksSection from "@/components/landing/HowItWorksSection";
+import StatsSection from "@/components/landing/StatsSection";
 import DeviceShowcaseSection from "@/components/landing/DeviceShowcaseSection";
 import DemoSection from "@/components/landing/DemoSection";
 import ProToolsSection from "@/components/landing/ProToolsSection";
@@ -30,6 +31,7 @@ export default function LandingPage() {
       <TrustedBySection />
       <FeaturesSection />
       <HowItWorksSection />
+      <StatsSection />
       <DeviceShowcaseSection />
       <DemoSection />
       <ProToolsSection />
