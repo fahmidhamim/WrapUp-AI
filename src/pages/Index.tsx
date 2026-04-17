0d5396827d1e8149e2d549691cc143d8922d1d47
Fahmid Hossain Hamim <fh.hamim18@gmail.com>
2026-04-17 13:00:15 +0600

    feat: move stats section under 'Built for teams' heading

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,8 +30,8 @@ export default function LandingPage() {
       <HeroScene />
       <TrustedBySection />
       <FeaturesSection />
-      <HowItWorksSection />
       <StatsSection />
+      <HowItWorksSection />
       <DeviceShowcaseSection />
       <DemoSection />
       <ProToolsSection />
