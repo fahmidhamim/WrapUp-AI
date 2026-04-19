cd0de8c54394218828c9d8b4435ccb48abf009c1
Fahmid Hossain Hamim <fh.hamim18@gmail.com>
2026-04-19 13:34:14 +0600

    fix(scroll): scope Lenis smooth-scroll to landing page so dashboard scrolling works

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,9 @@
 import { createRoot } from "react-dom/client";
-import Lenis from "lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import App from "./App.tsx";
 import "./index.css";
 
 gsap.registerPlugin(ScrollTrigger);
-
-const lenis = new Lenis({
-  duration: 1.4,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-});
-
-lenis.on("scroll", ScrollTrigger.update);
-
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
-
-gsap.ticker.lagSmoothing(0);
 
 createRoot(document.getElementById("root")!).render(<App />);
